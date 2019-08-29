252bdb8a0891a5f9bf8a7806f3f86115031db816
Adauto Fernandes <adauto.fernandes@mttechne.com.br>
2019-08-29 07:52:43 -0300

    Estrutura do projeto
    Inclusão de projeto SpringBoot criado https://start.spring.io/.  E criado a estrutura de onde vai ficar o front em angular.

---
+++ consult-api/src/main/java/br/com/atocf/consultapi/ConsultApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.atocf.consultapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultApiApplication.class, args);
+	}
+
+}
